297568db62b11e2b1512a64cf1a251e6d5abd092
Rohit-2301 <22bcs13677@cuchd.in>
2025-03-19 11:55:07 +0530

    Create IncreasingArray.cpp

---
+++ IncreasingArray.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minOperations(vector<int>& nums) {
+    int operations = 0;
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] <= nums[i - 1]) {
+            operations += (nums[i - 1] - nums[i] + 1);
+            nums[i] = nums[i - 1] + 1;
+        }
+    }
+    return operations;
+}
